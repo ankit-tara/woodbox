d642aa7a8f2e0816dde64a07a3a158c1ea8f83a5
rajneet <aneet.crazy@gmail.com>
2020-08-07 22:30:11 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -15,5 +15,14 @@ module.exports = {
   },
   env: {
     api_url: process.env.API_URL,
+    API_URL: process.env.API_URL,
+    SENDER_ID: process.env.SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    AUTH_KEY: process.env.AUTH_KEY,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    RAZOR_PAY_KEY_ID: process.env.RAZOR_PAY_KEY_ID,
+    RAZOR_PAY_KEY_SECRET: process.env.RAZOR_PAY_KEY_SECRET,
+    APP_URL: process.env.APP_URL
   },
 };
