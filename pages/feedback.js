f2b8c5067d3ab56a8dd4481292a4440eb8434783
Ankit Tara <ankittara15@gmail.com>
2020-08-21 00:03:11 +0530

    feedback page

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,29 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Layout from "../src/Layout";
-import { Box, Button, Container, Grid, Typography } from "@material-ui/core";
-import {
-  commonStyles,
-  desktopStyles,
-  mobileStyles,
-  TabStyles,
-} from "../src/styles";
+import StarRatings from "react-star-ratings";
+import { Container, Card, CardContent, Box, Typography, Link } from "@material-ui/core";
+import { getAllFeedback } from "../src/apis/global-api";
+
 
 const useStyles = makeStyles((theme) => ({
-  ...commonStyles,
-  [theme.breakpoints.up("md")]: desktopStyles,
-  [theme.breakpoints.between("sm", "md")]: TabStyles,
-  [theme.breakpoints.down("sm")]: mobileStyles,
+  card: {
+    marginBottom: "1rem",
+    boxShadow: "none",
+  },
+  cardBody: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  image: {
+    width: "100px",
+    height: "100px",
+    objectFit: "cover",
+    borderRadius: "50%",
+    marginRight: "1rem",
+    flexShrink: "0",
+  },
+  reviewContent: {
+    flex: "1",
+  },
+  heading:{
+    fontSize: '3rem',
+    textAlign: 'center',
+    margin: '1rem 0',
+    [theme.breakpoints.down("sm")]:{
+      fontSize: '2.5rem'
+    }
+  }
 }));
 
 export default function Feedback() {
   const classes = useStyles();
+  const [list_reviews, setlist_reviews] = useState([]);
+
+  useEffect(() => {
+    getAllFeedback().then((data) => {
+      setlist_reviews(data);
+    });
+  }, []);
 
   return (
     <Layout>
-      <section className={classes.section} style={{ textAlign: "center" }}>
-        <img className={classes.csoonImg} src="/static/images/csoon.svg" />
-        <Typography variant="h2">Page is Under Development</Typography>
+      <section className={classes.section}>
+        <Container maxWidth="xl">
+          <Typography variant="h3" className={classes.heading}>Reviews</Typography>
+          {list_reviews &&
+            list_reviews.length > 0 &&
+            list_reviews.map((item) => (
+              <div className={classes.TestimonialSlide} key={item.id}>
+                <Card className={classes.card}>
+                  <CardContent className={classes.cardBody}>
+                    <img src={item.link} alt="" className={classes.image} />
+                    <Box className={classes.reviewContent}>
+                      <Typography variant="h6" className={classes.name}>
+                        {item.name}
+                      </Typography>
+                      <StarRatings
+                        rating={item.rating}
+                        starRatedColor="#FFC107"
+                        starHoverColor="#FFC107"
+                        starDimension="21px"
+                        numberOfStars={5}
+                        name={item.id}
+                      />
+                      <Typography className={classes.review}>
+                        {item.text}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+        </Container>
       </section>
     </Layout>
   );
