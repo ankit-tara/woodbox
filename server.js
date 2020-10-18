2379ce735c12f73e6ce2fff5babbe1c62478b1d8
rajneet <aneet.crazy@gmail.com>
2020-10-18 20:00:32 +0530

    updates

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const app = require('express')()
-const server = require('http').Server(app)
+const app = require("express")();
+const server = require("http").Server(app);
 // const io = require('socket.io').listen(server)
-const next = require('next')
-const Razorpay = require("razorpay")
-const request = require('request');
-const serveStatic = require('serve-static')
+const next = require("next");
+const Razorpay = require("razorpay");
+const request = require("request");
+const serveStatic = require("serve-static");
 
-const dev = process.env.NODE_ENV !== 'production'
-const nextApp = next({ dev })
-const nextHandler = nextApp.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
 const cors = require("cors");
 const instance = new Razorpay({
-    key_id: process.env.RAZOR_PAY_KEY_ID,
-    key_secret: process.env.RAZOR_PAY_KEY_SECRET,
+  key_id: process.env.RAZOR_PAY_KEY_ID,
+  key_secret: process.env.RAZOR_PAY_KEY_SECRET,
 });
 // fake DB
 // const messages = []
@@ -28,114 +28,116 @@ const instance = new Razorpay({
 // })
 
 nextApp.prepare().then(() => {
+  app.use(cors());
+  // app.all('*', function (req, res, next) {
+  //     c
+  //     var origin = req.get('origin');
+  //     res.header('Access-Control-Allow-Origin', origin);
+  //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  //     res.header('Access-Control-Allow-Headers', 'Content-Type');
+  //     next();
+  // });
 
-    app.use(cors());
-    // app.all('*', function (req, res, next) {
-    //     c
-    //     var origin = req.get('origin');
-    //     res.header('Access-Control-Allow-Origin', origin);
-    //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    //     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    //     next();
-    // });
+  // app.get("/products/type/:type", (req, res) => {
+  //   let query = req.query ? req.query : {};
+  //   query.type = req.params.type;
+  //   // console.log(query, req.params);
 
-
-    // app.get("/products/type/:type", (req, res) => {
-    //   let query = req.query ? req.query : {};
-    //   query.type = req.params.type;
-    //   // console.log(query, req.params);
-
-    //   return app.render(req, res, "/products", query);
-    // });
-    // app.get("/products/category/:slug", (req, res) => {
-    //   return app.render(req, res, "/products", req.query);
-    // });
-
-    app.get('/service-worker.js', (req, res) => {
-        nextApp.serveStatic(req, res, './.next/service-worker.js')
+  //   return app.render(req, res, "/products", query);
+  // });
+  // app.get("/products/category/:slug", (req, res) => {
+  //   return app.render(req, res, "/products", req.query);
+  // });
+  app.use(
+    "/static",
+    express.static(__dirname + "/static", {
+      maxAge: "365d",
     })
+  );
+  app.get("/service-worker.js", (req, res) => {
+    nextApp.serveStatic(req, res, "./.next/service-worker.js");
+  });
 
-    const serviceWorkers = [
-        {
-            filename: 'service-worker.js',
-            path: './.next/service-worker.js',
-        },
-        {
-            filename: 'firebase-messaging-sw.js',
-            path: './public/static/firebase-messaging-sw.js',
-        },
-    ]
+  const serviceWorkers = [
+    {
+      filename: "service-worker.js",
+      path: "./.next/service-worker.js",
+    },
+    {
+      filename: "firebase-messaging-sw.js",
+      path: "./public/static/firebase-messaging-sw.js",
+    },
+  ];
 
-    serviceWorkers.forEach(({ filename, path }) => {
-        app.get(`/${filename}`, (req, res) => {
-            nextApp.serveStatic(req, res, path)
-        })
-    })
-
-    app.get("/order/:price/:reciept_id", (req, res) => {
-        try {
-            const options = {
-                amount: req.params.price, // amount == Rs 10
-                currency: "INR",
-                receipt: req.params.reciept_id,
-                payment_capture: 0,
-                // 1 for automatic capture // 0 for manual capture
-            };
-            instance.orders.create(options, async function (err, order) {
-                if (err) {
-                    return res.status(200).json({
-                        message: "Something Went Wrong 3 here",
-                        err: err
-                    });
-                }
-                return res.status(200).json(order);
-            });
-        } catch (err) {
-            return res.status(500).json({
-                message: "Something Went Wrong 4",
-                err: err
-            });
-        }
+  serviceWorkers.forEach(({ filename, path }) => {
+    app.get(`/${filename}`, (req, res) => {
+      nextApp.serveStatic(req, res, path);
     });
+  });
 
-    app.post("/capture/:paymentId/:price", (req, res) => {
-
-        try {
-            return request(
-                {
-                    method: "POST",
-                    url: `https://${process.env.RAZOR_PAY_KEY_ID}:${process.env.RAZOR_PAY_KEY_SECRET}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
-                    form: {
-                        amount: req.params.price, // amount == Rs 10 // Same As Order amount
-                        currency: "INR",
-                    },
-                },
-                async function (err, response, body) {
-                    // if (err) {
-                    //   return res.status(500).json({
-                    //     message: "Something Went Wrong 1",
-                    //   });
-                    // }
-
-                    return res.status(200).json(body);
-                }
-            );
-        } catch (err) {
-            return res.status(500).json({
-                message: "Something Went Wrong 2" + err,
-            });
+  app.get("/order/:price/:reciept_id", (req, res) => {
+    try {
+      const options = {
+        amount: req.params.price, // amount == Rs 10
+        currency: "INR",
+        receipt: req.params.reciept_id,
+        payment_capture: 0,
+        // 1 for automatic capture // 0 for manual capture
+      };
+      instance.orders.create(options, async function (err, order) {
+        if (err) {
+          return res.status(200).json({
+            message: "Something Went Wrong 3 here",
+            err: err,
+          });
         }
-    });
+        return res.status(200).json(order);
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: "Something Went Wrong 4",
+        err: err,
+      });
+    }
+  });
 
-    // app.get("/b", (req, res) => {
-    //   return app.render(req, res, "/b", req.query);
-    // });
-    app.get('*', (req, res, next) => {
-        // next();
-        return nextHandler(req, res, next)
-    })
-    server.listen(3000, (err) => {
-        if (err) throw err
-        console.log('> Ready on http://localhost:3000')
-    })
-})
+  app.post("/capture/:paymentId/:price", (req, res) => {
+    try {
+      return request(
+        {
+          method: "POST",
+          url: `https://${process.env.RAZOR_PAY_KEY_ID}:${process.env.RAZOR_PAY_KEY_SECRET}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
+          form: {
+            amount: req.params.price, // amount == Rs 10 // Same As Order amount
+            currency: "INR",
+          },
+        },
+        async function (err, response, body) {
+          // if (err) {
+          //   return res.status(500).json({
+          //     message: "Something Went Wrong 1",
+          //   });
+          // }
+
+          return res.status(200).json(body);
+        }
+      );
+    } catch (err) {
+      return res.status(500).json({
+        message: "Something Went Wrong 2" + err,
+      });
+    }
+  });
+
+  // app.get("/b", (req, res) => {
+  //   return app.render(req, res, "/b", req.query);
+  // });
+  app.get("*", (req, res, next) => {
+    // next();
+    return nextHandler(req, res, next);
+  });
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3000");
+  });
+});
